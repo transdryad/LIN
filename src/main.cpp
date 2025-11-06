a94557fd01b9ced572ed0c6c873cd680a9f61f65
Hazel Viswanath <viswanath.hazel@gmail.com>
2025-11-06 15:55:34 -0700

    words with accents

--- src/main.cpp
+++ src/main.cpp
@@ -7,14 +7,19 @@
 
 std::vector<std::string> word_list;
 
+void run(std::string source) {
+    
+}
+
 int main(int argc, char **argv) {
-    if (argc != 1) {
-        std::cout << argv[0] << " takes no arguments.\n";
+    if (argc != 2) {
+        std::cout << argv[0] << " takes one argument: the file to be compiled.\n";
         return 1;
     }
+    std::string input_file = 
     std::cout << "This is project " << PROJECT_NAME << ".\n";
 
-    std::ifstream wordlists("src/ODS8.txt"); //read wordlist into vector
+    std::ifstream wordlists("src/fr.txt"); //read wordlist into vector
     if (!wordlists.is_open()) { std::cerr << "Error reading the word list!"; return 1; }
     std::string s;
     while (getline(wordlists, s)) word_list.push_back(s);
---
+++ src/token.hpp
@@ -0,0 +1,8 @@
+#pragma once
+
+enum TokenType {
+    NE, PAS, // ( or {, ) or }
+    VIRGULE, PÉRIODE, SOUSTRAIRE, ADDITIONNER, DIVISER, MULTIPLIER, RESTER, // comma, dot, -, +, /, *, %
+    TYPE, REFLEXIVE, APPELLER,
+    NUMERO,
+};
