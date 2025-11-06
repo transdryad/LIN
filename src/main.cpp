2c76b0b02b3ccccf0639857043c55328d644d6b6
Hazel Viswanath <viswanath.hazel@gmail.com>
2025-11-05 18:38:33 -0700

    read word list into vector

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,11 @@
 #include <iostream>
+#include <vector>
+#include <fstream>
+#include <string>
 
 #define PROJECT_NAME "LIN"
+
+std::vector<std::string> word_list;
 
 int main(int argc, char **argv) {
     if (argc != 1) {
@@ -8,5 +13,16 @@ int main(int argc, char **argv) {
         return 1;
     }
     std::cout << "This is project " << PROJECT_NAME << ".\n";
+
+    std::ifstream wordlists("src/ODS8.txt"); //read wordlist into vector
+    if (!wordlists.is_open()) { std::cerr << "Error reading the word list!"; return 1; }
+    std::string s;
+    while (getline(wordlists, s)) word_list.push_back(s);
+    wordlists.close();
+
+    for (std::string str : word_list) {
+        std::cout << str << std::endl;
+    }
+
     return 0;
 }
